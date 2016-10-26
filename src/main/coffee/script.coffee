f2facecf2a5d0348f205890cebfb5dc79f3972f7
skup5 <5skup5@seznam.cz>
2016-10-26 14:21:10 +0200

    check in script if btnCardNo and btnBankAccount exist

--- src/main/coffee/script.coffee
+++ src/main/coffee/script.coffee
@@ -1,8 +1,10 @@
 
 $(document).ready ->
   $('[data-toggle="tooltip"]').tooltip()
-  document.getElementById("btnCardNo").onclick = () ->
+  btn = document.getElementById("btnCardNo")
+  if btn? then btn.onclick = () ->
     document.getElementById("inputCardNo").value = Math.floor(Math.random() * 9999999999999999) + 1
-  document.getElementById("btnBankAccountNo").onclick = () ->
+  btn = document.getElementById("btnBankAccountNo")
+  if btn? then btn.onclick = () ->
     document.getElementById("inputBankAccountNo").value = Math.floor(Math.random() * 999999999) + 1
   return
--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
+  var btn;
   $('[data-toggle="tooltip"]').tooltip();
-  document.getElementById("btnCardNo").onclick = function() {
-    return document.getElementById("inputCardNo").value = Math.floor(Math.random() * 9999999999999999) + 1;
-  };
-  document.getElementById("btnBankAccountNo").onclick = function() {
-    return document.getElementById("inputBankAccountNo").value = Math.floor(Math.random() * 999999999) + 1;
-  };
+  btn = document.getElementById("btnCardNo");
+  if (btn != null) {
+    btn.onclick = function() {
+      return document.getElementById("inputCardNo").value = Math.floor(Math.random() * 9999999999999999) + 1;
+    };
+  }
+  btn = document.getElementById("btnBankAccountNo");
+  if (btn != null) {
+    btn.onclick = function() {
+      return document.getElementById("inputBankAccountNo").value = Math.floor(Math.random() * 999999999) + 1;
+    };
+  }
 });
